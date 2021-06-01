58782bf6189f32ecf65f35cbefe18d9b344688c3
HappyKumamoto <67646107+HappyKumamoto@users.noreply.github.com>
2021-06-01 17:17:26 +0900

    Add files via upload

---
+++ onclick2.js
@@ -0,0 +1,42 @@
+let startTime = null;//startTime変数
+let displayArea = document.getElementById('display-area');
+let onclickArea = document.getElementById('onclick-area');
+
+function start(){
+    startTime = Date.now();//上で用意した変数startTimeへ現在時刻のミリ秒を代入
+    document.body.onclick = stop;
+}
+
+function stop() {
+    let currentTime = Date.now();
+    let seconds = (currentTime - startTime) / 1000;
+    if ( 7 <= seconds && seconds < 8 ) {
+        displayArea.innerText = `${seconds}秒でした！
+        おめでとうございます！！
+        ＼(^o^)／`;
+    }else{
+        displayArea.innerText = `${seconds}秒でした！
+        (*>ω<)おしいです！ `;
+    }
+  document.body.onclick = null;
+}
+
+if(confirm(`  👉[[  OK  ]] ➡  ➡  ➡  ➡  ➡  ➡  ➡  7秒  👉 [[  ☁️  ]]  `)){
+　　　　　//confirm関数は、戻り値が意味を持ち、OKを押されるとtrue、
+                  //キャンセルが押されるとfalseが返る
+                  
+        //let messages = [' &#x1f388; ' , ' &#x1fa81; ',' &#x26bd; ' ]; //innerHTMLで表示
+        let messages = [' 💡 ' , ' 🌧️ ',' 🍌 ', ' 🏐 ',' 🎈 ', '⏱ ','  🌵 ' , '☁️ '  , ' ⏳ ' , ' ❄ ' , ' 🌈 '  , ' 🥚 ' ];
+        let messageNo = Math.floor( Math.random() * messages.length);
+        console.log(messages[messageNo]);
+        document.getElementById('onclick-area').innerText = messages[messageNo];
+       //HTML EntityはinnerHTMLで絵文字表示されるが、動かない
+      　　　//絵文字はinnerTextで      同上
+
+  start();
+}
+
+/*document.getElementById("emoji").addEventListener("click",function(){
+    document.getElementById("emoji").textContent = "クリックされました。";
+    alert("かおもじがクリックされました。")
+});*/
